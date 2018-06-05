2716376185964a3fc5be19cd35297808f8ba687a
Egon Elbre <egonelbre@gmail.com>
2018-06-05 23:16:31 +0300

    Implement TexRef and SurfRef.
    Fixes gorgonia/cu#12
    Fixes gorgonia/cu#11

--- surfref.go
+++ surfref.go
@@ -2,6 +2,7 @@ package cu
 
 // #include <cuda.h>
 import "C"
+import "unsafe"
 
 type SurfRef struct {
 	ref C.CUsurfref
@@ -9,4 +10,20 @@ type SurfRef struct {
 
 func (s SurfRef) c() C.CUsurfref {
 	return s.ref
+}
+
+func (mod Module) SurfRef(name string) (SurfRef, error) {
+	var ref SurfRef
+	cname := C.CString(name)
+	defer C.free(unsafe.Pointer(cname))
+	err := result(C.cuModuleGetSurfRef(&ref.ref, mod.mod, cname))
+	return ref, err
+}
+
+func (ctx *Ctx) ModuleSurfRef(mod Module, name string) (SurfRef, error) {
+	var ref SurfRef
+	cname := C.CString(name)
+	defer C.free(unsafe.Pointer(cname))
+	err := result(C.cuModuleGetSurfRef(&ref.ref, mod.mod, cname))
+	return ref, err
 }
